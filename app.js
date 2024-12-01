92adf6e69a75ae82509a3781b04cb351693dc272
Ali Raza <161970819+ALiRazax1@users.noreply.github.com>
2024-12-02 00:05:55 +0500

    upload images

--- app.js
+++ app.js
@@ -1,35 +1,35 @@
 let mobilePhone = {
     Realme: {
         "Realme 13 Plus 5G": {name:"Realme 13 Plus 5G" ,  RAM: "12GB", Storage: "256GB", Processor: "Dimensity 7300", Price: "PKR 73,999", Battery: "5000mAh", Display: "6.67-inch AMOLED, 120Hz", Camera: "50MP + 2MP (Rear), 16MP (Front)" , image: "assets/realme-13-plus-5g-pakistan.jpg" },
-        "Realme 13 5G": {name: "Realme 13 5G",   RAM: "8GB", Storage: "128GB", Processor: "Dimensity 6300", Price: "PKR 55,000", Battery: "5000mAh", Display: "6.72-inch AMOLED, 120Hz", Camera: "50MP + 2MP (Rear), 16MP (Front)" , image: "" },
-        "Realme Narzo 70": {name: "Realme Narzo 70",   RAM: "6GB", Storage: "128GB", Processor: "Dimensity 810", Price: "PKR 44,999", Battery: "4500mAh", Display: "6.5-inch LCD, 90Hz", Camera: "48MP + 2MP (Rear), 8MP (Front)" , image: "" },
-        "Realme C25s": {name:"Realme C25s",   RAM: "4GB", Storage: "64GB", Processor: "Helio G85", Price: "PKR 25,999", Battery: "6000mAh", Display: "6.5-inch LCD", Camera: "13MP + 2MP + 2MP" , image: "" },
-        "Realme C51": {name:"Realme C51",   RAM: "4GB", Storage: "64GB", Processor: "Unisoc T612", Price: "PKR 28,999", Battery: "5000mAh", Display: "6.74-inch LCD", Camera: "50MP + 0.08MP" , image: "" },
-        "Realme Narzo 50": {name: "Realme Narzo 50",   RAM: "6GB", Storage: "128GB", Processor: "Helio G96", Price: "PKR 29,999", Battery: "5000mAh", Display: "6.6-inch LCD, 120Hz", Camera: "50MP + 2MP" , image: "" }
+        "Realme 13 5G": {name: "Realme 13 5G",   RAM: "8GB", Storage: "128GB", Processor: "Dimensity 6300", Price: "PKR 55,000", Battery: "5000mAh", Display: "6.72-inch AMOLED, 120Hz", Camera: "50MP + 2MP (Rear), 16MP (Front)" , image: "assets/realme-13-plus-5g-pakistan.jpg" },
+        "Realme Narzo 70": {name: "Realme Narzo 70",   RAM: "6GB", Storage: "128GB", Processor: "Dimensity 810", Price: "PKR 44,999", Battery: "4500mAh", Display: "6.5-inch LCD, 90Hz", Camera: "48MP + 2MP (Rear), 8MP (Front)" , image: "assets/realme-narzo-70-pro-narzo70p-4.webp" },
+        "Realme C25s": {name:"Realme C25s",   RAM: "4GB", Storage: "64GB", Processor: "Helio G85", Price: "PKR 25,999", Battery: "6000mAh", Display: "6.5-inch LCD", Camera: "13MP + 2MP + 2MP" , image: "assets/realme-c25s.jpg" },
+        "Realme C51": {name:"Realme C51",   RAM: "4GB", Storage: "64GB", Processor: "Unisoc T612", Price: "PKR 28,999", Battery: "5000mAh", Display: "6.74-inch LCD", Camera: "50MP + 0.08MP" , image: "assets/realme-c51.webp" },
+        "Realme Narzo 50": {name: "Realme Narzo 50",   RAM: "6GB", Storage: "128GB", Processor: "Helio G96", Price: "PKR 29,999", Battery: "5000mAh", Display: "6.6-inch LCD, 120Hz", Camera: "50MP + 2MP" , image: "assets/realme-narzo-70-pro-narzo70p-4.webp" }
     },
     Samsung: {
-        "Galaxy S23 Ultra": {name: "Galaxy S23 Ultra",   RAM: "12GB", Storage: "256GB", Processor: "Snapdragon 8 Gen 2", Price: "PKR 389,999", Battery: "5000mAh", Display: "6.8-inch AMOLED, 120Hz", Camera: "200MP + 12MP + 10MP + 10MP" , image: "" },
-        "Galaxy A14": {name: "Galaxy A14",   RAM: "4GB", Storage: "64GB", Processor: "Exynos 850", Price: "PKR 32,999", Battery: "5000mAh", Display: "6.6-inch PLS LCD", Camera: "50MP + 2MP + 2MP" , image: "" },
-        "Galaxy M13 5G": {name:"Galaxy M13 5G",   RAM: "6GB", Storage: "128GB", Processor: "Dimensity 700", Price: "PKR 34,128", Battery: "5000mAh", Display: "6.5-inch HD+ LCD", Camera: "50MP + 2MP" , image: "" },
-        "Samsung Galaxy A05": {name: "Samsung Galaxy A05",   RAM: "4GB", Storage: "128GB", Processor: "Exynos 850", Price: "PKR 26,500", Battery: "5000mAh", Display: "6.75-inch LCD", Camera: "50MP + 2MP" , image: "" },
-        "Samsung Galaxy A03": {name:"Samsung Galaxy A03",   RAM: "3GB", Storage: "64GB", Processor: "Unisoc T606", Price: "PKR 28,500", Battery: "5000mAh", Display: "6.5-inch PLS TFT", Camera: "48MP + 2MP" , image: "" },
-        "Samsung Galaxy M04": {name:"Samsung Galaxy M04",   RAM: "4GB", Storage: "64GB", Processor: "Helio P35", Price: "PKR 29,999", Battery: "5000mAh", Display: "6.5-inch PLS LCD", Camera: "13MP + 2MP" , image: "" }
+        "Galaxy S23 Ultra": {name: "Galaxy S23 Ultra",   RAM: "12GB", Storage: "256GB", Processor: "Snapdragon 8 Gen 2", Price: "PKR 389,999", Battery: "5000mAh", Display: "6.8-inch AMOLED, 120Hz", Camera: "200MP + 12MP + 10MP + 10MP" , image: "assets/samsung-galaxy-s23-ultra.jpg" },
+        "Galaxy A14": {name: "Galaxy A14",   RAM: "4GB", Storage: "64GB", Processor: "Exynos 850", Price: "PKR 32,999", Battery: "5000mAh", Display: "6.6-inch PLS LCD", Camera: "50MP + 2MP + 2MP" , image: "assets/samsung-galaxy-a14.jpg" },
+        "Galaxy M13 5G": {name:"Galaxy M13 5G",   RAM: "6GB", Storage: "128GB", Processor: "Dimensity 700", Price: "PKR 34,128", Battery: "5000mAh", Display: "6.5-inch HD+ LCD", Camera: "50MP + 2MP" , image: "assets/Samsung-Galaxy-M13-5G.png" },
+        "Samsung Galaxy A05": {name: "Samsung Galaxy A05",   RAM: "4GB", Storage: "128GB", Processor: "Exynos 850", Price: "PKR 26,500", Battery: "5000mAh", Display: "6.75-inch LCD", Camera: "50MP + 2MP" , image: "assets/samsung-glaxy-a05.jpg" },
+        "Samsung Galaxy A03": {name:"Samsung Galaxy A03",   RAM: "3GB", Storage: "64GB", Processor: "Unisoc T606", Price: "PKR 28,500", Battery: "5000mAh", Display: "6.5-inch PLS TFT", Camera: "48MP + 2MP" , image: "assets/Samsung-Galaxy-A03.jpg" },
+        "Samsung Galaxy M04": {name:"Samsung Galaxy M04",   RAM: "4GB", Storage: "64GB", Processor: "Helio P35", Price: "PKR 29,999", Battery: "5000mAh", Display: "6.5-inch PLS LCD", Camera: "13MP + 2MP" , image: "assets/Samsung-Galaxy-M04.png" }
     },
     Xiaomi: {
-        "Xiaomi 13 Pro": {name:"Xiaomi 13 Pro",   RAM: "12GB", Storage: "256GB", Processor: "Snapdragon 8 Gen 2", Price: "PKR 279,999", Battery: "4820mAh", Display: "6.73-inch AMOLED, 120Hz", Camera: "50MP + 50MP + 50MP" , image: "" },
-        "Redmi Note 12 Pro": {name:"Redmi Note 12 Pro",   RAM: "8GB", Storage: "128GB", Processor: "Dimensity 1080", Price: "PKR 64,999", Battery: "5000mAh", Display: "6.67-inch AMOLED, 120Hz", Camera: "108MP + 8MP + 2MP" , image: "" },
-        "Poco X5 Pro": {name:"Poco X5 Pro",   RAM: "6GB", Storage: "128GB", Processor: "Snapdragon 778G", Price: "PKR 59,999", Battery: "5000mAh", Display: "6.67-inch AMOLED, 120Hz", Camera: "108MP + 8MP + 2MP" , image: "" },
-        "Xiaomi Redmi 13C": {name:"Xiaomi Redmi 13C",   RAM: "4GB", Storage: "128GB", Processor: "Helio G85", Price: "PKR 25,999", Battery: "5000mAh", Display: "6.74-inch LCD", Camera: "50MP + 2MP" , image: "" },
-        "Xiaomi Redmi 12C": {name:"Xiaomi Redmi 12C",   RAM: "6GB", Storage: "128GB", Processor: "Helio G88", Price: "PKR 27,999", Battery: "5000mAh", Display: "6.67-inch AMOLED", Camera: "8MP" , image: "" },
-        "Xiaomi Poco C55": {name:"Xiaomi Poco C55",   RAM: "4GB", Storage: "64GB", Processor: "Helio G85", Price: "PKR 24,999", Battery: "5000mAh", Display: "6.52-inch LCD", Camera: "50MP + 2MP" , image: "" }
+        "Xiaomi 13 Pro": {name:"Xiaomi 13 Pro",   RAM: "12GB", Storage: "256GB", Processor: "Snapdragon 8 Gen 2", Price: "PKR 279,999", Battery: "4820mAh", Display: "6.73-inch AMOLED, 120Hz", Camera: "50MP + 50MP + 50MP" , image: "assets/Xiaomi-13-Pro.webp" },
+        "Redmi Note 12 Pro": {name:"Redmi Note 12 Pro",   RAM: "8GB", Storage: "128GB", Processor: "Dimensity 1080", Price: "PKR 64,999", Battery: "5000mAh", Display: "6.67-inch AMOLED, 120Hz", Camera: "108MP + 8MP + 2MP" , image: "assets/redmi-note-12-pro.webp" },
+        "Poco X5 Pro": {name:"Poco X5 Pro",   RAM: "6GB", Storage: "128GB", Processor: "Snapdragon 778G", Price: "PKR 59,999", Battery: "5000mAh", Display: "6.67-inch AMOLED, 120Hz", Camera: "108MP + 8MP + 2MP" , image: "assets/poco-x5-pro.webp" },
+        "Xiaomi Redmi 13C": {name:"Xiaomi Redmi 13C",   RAM: "4GB", Storage: "128GB", Processor: "Helio G85", Price: "PKR 25,999", Battery: "5000mAh", Display: "6.74-inch LCD", Camera: "50MP + 2MP" , image: "assets/Xiaomi-Redmi-13C.jpg" },
+        "Xiaomi Redmi 12C": {name:"Xiaomi Redmi 12C",   RAM: "6GB", Storage: "128GB", Processor: "Helio G88", Price: "PKR 27,999", Battery: "5000mAh", Display: "6.67-inch AMOLED", Camera: "8MP" , image: "assets/Xiaomi-Redmi-12C.jpg" },
+        "Xiaomi Poco C55": {name:"Xiaomi Poco C55",   RAM: "4GB", Storage: "64GB", Processor: "Helio G85", Price: "PKR 24,999", Battery: "5000mAh", Display: "6.52-inch LCD", Camera: "50MP + 2MP" , image: "assets/xiaomi-poco-c55.jpg" }
     },
     Tecno: {
-        "Camon 20 Pro": {name:"Camon 20 Pro",   RAM: "8GB", Storage: "128GB", Processor: "Helio G99", Price: "PKR 47,999", Battery: "5000mAh", Display: "6.67-inch AMOLED, 120Hz", Camera: "64MP + 2MP + 2MP" , image: "" },
-        "Spark 10 Pro": {name:"Spark 10 Pro",   RAM: "8GB", Storage: "128GB", Processor: "Helio G88", Price: "PKR 32,999", Battery: "5000mAh", Display: "6.8-inch LCD, 90Hz", Camera: "50MP + AI Lens" , image: "" },
-        "Phantom X2": {name:"Phantom X2",   RAM: "8GB", Storage: "256GB", Processor: "Dimensity 9000", Price: "PKR 89,999", Battery: "5160mAh", Display: "6.8-inch AMOLED, 120Hz", Camera: "64MP + 13MP + 2MP" , image: "" },
-        "Tecno Spark 20C": {name:"Tecno Spark 20C",   RAM: "4GB", Storage: "128GB", Processor: "Helio G85", Price: "PKR 27,999", Battery: "5000mAh", Display: "6.6-inch LCD", Camera: "50MP + 0.08MP" , image: "" },
-        "Tecno Pop 8": {name:"Tecno Pop 8",   RAM: "3GB", Storage: "64GB", Processor: "Helio A22", Price: "PKR 24,999", Battery: "5000mAh", Display: "6.52-inch LCD", Camera: "13MP + QVGA" , image: "" },
-        "Tecno Pop 6 Pro": {name:"Tecno Pop 6 Pro",   RAM: "3GB", Storage: "64GB", Processor: "Helio A22", Price: "PKR 28,999", Battery: "5000mAh", Display: "6.56-inch LCD", Camera: "13MP" , image: "" }
+        "Camon 20 Pro": {name:"Camon 20 Pro",   RAM: "8GB", Storage: "128GB", Processor: "Helio G99", Price: "PKR 47,999", Battery: "5000mAh", Display: "6.67-inch AMOLED, 120Hz", Camera: "64MP + 2MP + 2MP" , image: "assets/Tecno-Camon-20-Pro.jpg" },
+        "Spark 10 Pro": {name:"Spark 10 Pro",   RAM: "8GB", Storage: "128GB", Processor: "Helio G88", Price: "PKR 32,999", Battery: "5000mAh", Display: "6.8-inch LCD, 90Hz", Camera: "50MP + AI Lens" , image: "assets/Tecno-Spark-10-Pro.jpg" },
+        "Phantom X2": {name:"Phantom X2",   RAM: "8GB", Storage: "256GB", Processor: "Dimensity 9000", Price: "PKR 89,999", Battery: "5160mAh", Display: "6.8-inch AMOLED, 120Hz", Camera: "64MP + 13MP + 2MP" , image: "assets/tecno-phantom-x2.webp" },
+        "Tecno Spark 20C": {name:"Tecno Spark 20C",   RAM: "4GB", Storage: "128GB", Processor: "Helio G85", Price: "PKR 27,999", Battery: "5000mAh", Display: "6.6-inch LCD", Camera: "50MP + 0.08MP" , image: "assets/Tecno-Spark-20C.webp" },
+        "Tecno Pop 8": {name:"Tecno Pop 8",   RAM: "3GB", Storage: "64GB", Processor: "Helio A22", Price: "PKR 24,999", Battery: "5000mAh", Display: "6.52-inch LCD", Camera: "13MP + QVGA" , image: "assets/Tecno-Pop-8.jpg" },
+        "Tecno Pop 6 Pro": {name:"Tecno Pop 6 Pro",   RAM: "3GB", Storage: "64GB", Processor: "Helio A22", Price: "PKR 28,999", Battery: "5000mAh", Display: "6.56-inch LCD", Camera: "13MP" , image: "assets/Tecno-Pop-6-Pro.webp" }
     }
 };
 
@@ -61,14 +61,15 @@ function selectProduct() {
 }
 let productCard = document.getElementById("product-card");
 function searchProduct() {
+  productCard.innerHTML = "";
   for (let keys in mobilePhone[selectedBrand]) {
     if (mobilePhone[selectedBrand][keys].name === selectedProduct) {
         let productDetails = mobilePhone[selectedBrand][keys];
-        console.log(productDetails);
+        
         
         productCard.innerHTML +=`<div class="">
         <div class="card" style="width: 18rem;">
-            <img src="${productDetails.image}" class="card-img-top" alt="...">
+            <img src="${productDetails.image}" class="w-100 card-img-top pt-3 px-3" alt="...">
             <div class="card-body">
               <h5 class="card-title">${productDetails.name}</h5>
               <p class="card-text">${productDetails.Price}</p>
